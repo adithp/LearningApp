ab1dc7c694db345ed3efe301e1e73195e3e0602f
adithp <adithkandoth@gmail.com>
2024-03-11 21:32:55 +0530

    Almost Complete last profile page not completed

--- src/components/Study.js
+++ src/components/Study.js
@@ -4,6 +4,8 @@ import React, { useEffect, useState } from 'react'
 
 import VideoRender from './VideoRender'
 import CompleteIcon from '../Assets/images/compleated.svg'
+import CompleteIconWhite from '../Assets/images/compleated2.svg'
+import PlayIconWhite from '../Assets/images/play1.svg'
 import DropDownIcon from '../Assets/images/drop-down.svg'
 import PlayIcon from '../Assets/images/play.svg'
 import LockIcon from '../Assets/images/lock.svg'
@@ -11,133 +13,143 @@ import LockIcon from '../Assets/images/lock.svg'
 export default function Study() {
 
 
-	
-
-	
-
 	const [lessons,setLessons] = useState([
-		[
-		{
-			id:0,
-			name:'Lesson 1',
-			title:'Indroduction',
-			complete:false,
-			part: 1001,
-		},
 		{
 			id:1,
+			part:1,
 			lock:false,
 			name:'Indroduction',
 			complete:true,
 		},
 		{
 			id:2,
+			part:1,
 			lock:false,
 			name:'fundamentals of maths',
 			complete:false,
 		},
 		{
 			id:3,
+			part:1,
 			lock:true,
 			name:'calculs',
 			complete:false,
 		},
 		{
 			id:4,
+			part:1,
 			lock:true,
 			name:'set theory',
 			complete:false,
 		},
 		{
 			id:5,
+			part:1,
 			lock:true,
 			name:'functions',
 			complete:false,
 		},
-		],
-		[
+		
 		{
 			id:6,
-			lock:true,
-			name:'Lesson 2',
-			title:'Matrix',
-			complete:false,
-			part:1002,
-		},
-
-		{
-			id:7,
+			part:2,
 			lock:true,
 			name:'Indroduction',
 			complete:false,
 		},
 		{
-			id:8,
+			id:7,
+			part:2,
 			lock:true,
 			name:'fundamentals of maths',
 			complete:false,
 		},
 		{
-			id:9,
+			id:8,
+			part:2,
 			lock:true,
 			name:'calculs',
 			complete:false,
 		},
 		{
-			id:10,
+			id:9,
+			part:2,
 			lock:true,
 			name:'set theory',
 			complete:false,
 		},
-		{
-			id:11,
+			{
+			id:10,
+			part:2,
 			lock:true,
 			name:'functions',
 			complete:false,
-		},],	
+		},
 	])
+
+	const [lessonPart,setLessonPart] = useState([1,2])
 	const initialVisibility = new Array(lessons.length).fill(false) ;
   	const [boxesVisibility, setBoxesVisibility] = useState(initialVisibility);
 	const [select,setSelect] = useState(2)
+
+
 	const toggleBoxVisibility = (index) => {
 		const updatedVisibility = [...boxesVisibility];
 		updatedVisibility[index] = !updatedVisibility[index];
 		setBoxesVisibility(updatedVisibility);
 	};
 	
-	const renderIcons = (item)=> {
+
+	const renderIcons = (item,color)=> {
 		if(item.complete == true) {
-			return <CompleteIcon />
+			if(color == true) {
+				return  <CompleteIconWhite /> 
+			}
+			else {
+				return <CompleteIcon />
+			}
+			
 		}
 		else if (item.lock == true) {
 			return <LockIcon />
 		}
 		else {
-			return  <PlayIcon /> 
+			if(color == true) {
+				return  <PlayIconWhite /> 
+			}
+			else {
+				return <PlayIcon />
+			}
 		}
 	}
+
+
 	const source = require('../Assets/videos/5.mp4')
 	const thumbnail = 'https://templates.simplified.co/thumb/444e5ae4-776c-4318-8a4d-566072d3083f.jpg'
+
+
 	const lessonRender = ()=> {
-		
-		return lessons.map((item,index)=> (
-			<View key={item[0].part}>
-			<TouchableOpacity  style={styles.LessonButton} onPress={() => toggleBoxVisibility(index)} >
-				<Text style={styles.LessonButtonText}>
-					{item[0].name}
-				</Text>
-				<DropDownIcon height={15} />
-			</TouchableOpacity>
-				{boxesVisibility[index] ? renderSubject(item) : null}	
-			</View>
-		))
+		return lessonPart.map((item,index)=> (
+			<View key={item}>
+		<TouchableOpacity  style={styles.LessonButton} onPress={() => toggleBoxVisibility(index)} >
+			<Text style={styles.LessonButtonText}>
+				Lesson{item}
+			</Text>
+			<DropDownIcon height={15} fill={'#fff'}/>
+		</TouchableOpacity>
+			{boxesVisibility[index] ? renderSubject(item) : null}	
+		</View>
+		)) 
 	};
+
+
 	const renderSubject = (item) => {
-		return item.slice(1).map((newitem,index)=> {
+		const renderItems = lessons.filter((newitem) => newitem.part == item)
+		return renderItems.map((newitem)=> {
 			if(newitem.id == select) {
 				return (
-					<TouchableOpacity key={newitem.id} style={[styles.subjectBox,{backgroundColor:'#726AEC'}]} >
-				{renderIcons(newitem)}
+					<TouchableOpacity key={newitem.id} style={[styles.subjectBox,{backgroundColor:'#726AEC'}]} onPress={()=> lessonChange(newitem)}>
+				{renderIcons(newitem,color=true)}
 				<Text style={[styles.subjectBoxText,{color:'#fff'}]}>{newitem.name}</Text>
 				<Text style={[styles.subjectBoxTimeText,{color:'#fff'}]}>2:05</Text>
 			</TouchableOpacity>
@@ -145,16 +157,35 @@ export default function Study() {
 			}
 			else {
 				return (
-					<TouchableOpacity key={newitem.id} style={[styles.subjectBox,]} >
-				{renderIcons(newitem)}
+					<TouchableOpacity key={newitem.id} style={[styles.subjectBox,]} onPress={()=> lessonChange(newitem)} >
+				{renderIcons(newitem,color=false)}
 				<Text style={styles.subjectBoxText}>{newitem.name}</Text>
 				<Text style={styles.subjectBoxTimeText}>2:05</Text>
 			</TouchableOpacity>
 				)
 			}
-		})
+				})
 		
 	};
+
+	const lessonChange = (instance)=> {
+		if(instance.lock == false ) {
+			setSelect(instance.id)
+		}
+	};
+
+	const markHandle = (id)=> {
+		const updatedLessons = [...lessons];
+		const currentLessonIndex = updatedLessons.findIndex(lesson => lesson.id === id);
+		if (currentLessonIndex !== -1 && currentLessonIndex < updatedLessons.length - 1) {
+			updatedLessons[currentLessonIndex].complete = true;
+			updatedLessons[currentLessonIndex + 1].lock = false;
+			setLessons(updatedLessons);
+			if(id != lessons.length-1 ) {
+				setSelect(select+1)
+			}
+		}
+	}
   return (
 	<>
     <SafeAreaView>
@@ -164,7 +195,7 @@ export default function Study() {
 				<Text style={styles.topMainText}>Indroduction</Text>
 				<Text style={styles.topSecondaryText}>Fundamentals of maths</Text>
 			</View>
-			<TouchableOpacity style={styles.markButton}>
+			<TouchableOpacity style={styles.markButton} onPress={()=> markHandle(select)}>
 				<CompleteIcon height={12} width={12} />
 				<Text style={styles.markButtonText}>Mark as Completed</Text>
 			</TouchableOpacity>
